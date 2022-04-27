668f2a2f3fa67459c9a524fd16782d7db2f21cd8
Karol <kurekdev@gmail.com>
2022-04-27 16:53:17 +0200

    delete detail-note

--- notes/urls.py
+++ notes/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('update-topic/<str:pk>/', views.update_topic, name='update-topic'),
     path('delete-topic/<str:pk>/', views.delete_topic, name='delete-topic'),
     path('<str:pk>/', views.topic_detail, name='topic-detail'),
-    path('<str:pk_topic>/<str:pk_note>', views.note_view, name='note-detail'),
     path('<str:pk>/create-note/', views.create_note, name='create-note'),
     path('<str:pk_topic>/update-note/<str:pk_note>', views.update_note, name='note-update'),
 ]
